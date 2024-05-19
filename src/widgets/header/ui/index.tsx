99327014f04900e470f5f075fa58da54f485a4cc
apashintsev <av.pashintsev@gmail.com>
2024-05-19 03:16:14 +0300

    ++

--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -3,85 +3,82 @@ import { useToogle } from 'shared/lib/toggle';
 import hooks from './assets/hooks.svg'
 import fish from './assets/fish.svg'
 import brill from './assets/brilliant.svg'
-import no from './assets/ind_no.svg'
-// import yes from './assets/ind_yes.svg'
+import no from "./assets/ind_no.svg";
+import ok from "./assets/ind_ok.svg";
+import yes from "./assets/ind_yes.svg";
 import line from './assets/prog_line.svg'
 import { Menu, InfoModal, DepositModal } from './components';
 
 import s from './style.module.scss'
-import { useUnit } from 'effector-react';
-import { $userProfile } from 'shared/config/user';
 import { useAppSelector } from 'hooks/redux';
 
 export const Header = () => {
-    const profile = useUnit($userProfile);
     const [isOpenMenu, onSetStateMenu] = useToogle();
     const [isOpenInfo, onSetStateInfo] = useToogle();
     const [isOpenDeposit, onSetStateDeposit] = useToogle();
-    const { activeTab } = useAppSelector((state) => state.appSlice);
-
+    const { activeTab,panelData } = useAppSelector((state) => state.appSlice);
+  function getIndValue(key: number): string {
+    const indValues: { [key: number]: string } = {
+      0: no,
+      1: ok,
+      2: yes,
+    };
+    return indValues[key] || no;
+  }
     return (
-      
-        <header className={activeTab === '1' ? [s.header_wrap,s.header_game].join(' '):s.header_wrap }>
-            <div className={s.header}>
-                <div className={s.points}>
-                    <img
-                        width='21px'
-                        height='21px'
-                        src={hooks}
-                    />
+      <header
+        className={
+          activeTab === "1"
+            ? [s.header_wrap, s.header_game].join(" ")
+            : s.header_wrap
+        }
+      >
+        <div className={s.header}>
+          <div className={s.points}>
+            <img width="21px" height="21px" src={hooks} />
 
-                    {profile?.referral_points ?? 0}
-                </div>
+            {panelData.bobberValue ?? 0}
+          </div>
 
-                <div className={s.points}>
-                    <img
-                        width='26px'
-                        height='25px'
-                        src={fish}
-                    />
+          <div className={s.points}>
+            <img width="26px" height="25px" src={fish} />
 
-                    {profile?.days_points ?? 0}
-                </div>
+            {panelData.fishValue ?? 0}
+          </div>
 
-                <div className={s.points}>
-                    <img
-                        width='23px'
-                        height='23px'
-                        src={brill}
-                    />
+          <div className={s.points}>
+            <img width="23px" height="23px" src={brill} />
 
-                    {profile?.days_points ?? 0}
-                </div>
+            {panelData.balance ?? 0}
+          </div>
 
-                <div onClick={onSetStateMenu} className={s.menu}>
-                    <div className={s.menu_item} />
-                    <div className={s.menu_item} />
-                    <div className={s.menu_item} />
-                </div>
-            </div>
-            {activeTab === '1' && <>
-        <div className={s.progress}>
-            <div className={s.line}>
-                <img src={line} className={s.line_img} />
-            </div>
-            <img src={no}
-            // {yes}
-            className={s.indicator_img} />
+          <div onClick={onSetStateMenu} className={s.menu}>
+            <div className={s.menu_item} />
+            <div className={s.menu_item} />
+            <div className={s.menu_item} />
+          </div>
         </div>
-      </>}
-            <Menu
-                onSetState={onSetStateMenu}
-                onSetModal={onSetStateInfo}
-                onSetStateDeposit={onSetStateDeposit}
-                isOpen={isOpenMenu}
-            />
-            <InfoModal
-                onSetState={onSetStateInfo}
-                isOpen={isOpenInfo}
-            />
-            <DepositModal  onSetState={onSetStateDeposit}
-                isOpen={isOpenDeposit}/>
-        </header>
-    )
+        {activeTab === "1" && (
+          <>
+            <div className={s.progress}>
+              <div className={s.line}>
+                <img src={line} className={s.line_img} />
+              </div>
+              <img
+                src={getIndValue(panelData.packsCount ?? 0)!}
+                className={s.indicator_img}
+              />
+            </div>
+          </>
+        )}
+        <Menu
+          onSetState={onSetStateMenu}
+          onSetModal={onSetStateInfo}
+          onSetStateDeposit={onSetStateDeposit}
+          isOpen={isOpenMenu}
+        />
+        <InfoModal onSetState={onSetStateInfo} isOpen={isOpenInfo} />
+        <DepositModal onSetState={onSetStateDeposit} isOpen={isOpenDeposit} />
+      </header>
+    );
 }
