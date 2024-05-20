07dcc32150679e8161c8af8e4aa717f4cfe2de87
Lifanovaa25 <50072172+Lifanovaa25@users.noreply.github.com>
2024-05-21 00:00:04 +0300

    copy

--- src/features/referral-link/ui/index.tsx
+++ src/features/referral-link/ui/index.tsx
@@ -4,31 +4,39 @@ import { useTranslation } from "react-i18next";
 import invite_icon from "./assets/invite_icon.svg";
 import { useAppDispatch, useAppSelector } from "hooks/redux";
 import { Leaderboard } from "widgets/leaderboard";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getRefData } from "store/apis";
 
 export const ReferralLink = () => {
   const { t } = useTranslation();
-  const { youInvitedCount, refLeaderboard, initDataRow } = useAppSelector(
+  const { youInvitedCount, refLeaderboard, initDataRow,refUrl } = useAppSelector(
     (state) => state.appSlice
   );
   const dispatch = useAppDispatch();
-
+  const [isBlinking, setIsBlinking] = useState(false);
   useEffect(() => {
     if (initDataRow) {
       dispatch(getRefData({ tma: initDataRow! }));
     }
   }, [initDataRow]);
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(
+        refUrl
+    );
+    setIsBlinking(true);
+    setTimeout(() => setIsBlinking(false), 500);
+};
   return (
     <>
       <div className={s.container}>
         <p className={s.title}>{t("invite_title")}</p>
         <div
-          //onClick={}
+          onClick={handleCopyClick}
           className={s.link}
         >
           <span
-            className={true ? [s.active, s.link_text].join(" ") : s.link_text}
+            className={isBlinking ?  s.link_text :s.active}
           >
             {t("Copy_invite_Link")}
           </span>
