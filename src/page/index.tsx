f3cabfcc2e0eb769aef7d91a37a9b415d816a26a
apashintsev <av.pashintsev@gmail.com>
2024-05-21 12:16:29 +0300

    ++

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -21,8 +21,10 @@ const DashBoardPage = () => {
   const { activeTab, /*, initDataRow */ lang} = useAppSelector(
     (state) => state.appSlice
   );
-  const { initDataRaw } = retrieveLaunchParams();
-  //const initDataRaw = "test"
+  //const { initDataRaw } = retrieveLaunchParams();
+  //console.log(initDataRaw)
+  const initDataRaw =
+    "query_id=AAHVXm8TAAAAANVebxP8QTqO&user=%7B%22id%22%3A326065877%2C%22first_name%22%3A%22Andrey%22%2C%22last_name%22%3A%22Vladimirovich%22%2C%22username%22%3A%22apashintsev%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1716282891&hash=5e6779d8b94d4ba5958540a84a1ef9b8d1497f0bd24cac7437b65c3172e1552d";
   const { setInitDataRow } = appSlice.actions;
   const dispatch = useAppDispatch();
 
