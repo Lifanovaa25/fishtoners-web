2541ea9f13be6fdc79b9048037c9d872f8ad61a4
Lifanovaa25 <50072172+Lifanovaa25@users.noreply.github.com>
2024-05-21 16:22:41 +0300

    loading

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -12,13 +12,13 @@ import { useAppDispatch, useAppSelector } from "hooks/redux";
 import { Game } from "features/game";
 import { ReferralLink } from "features/referral-link";
 import { FirstVizit } from "features/firstVisit";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // import { retrieveLaunchParams } from "@tma.js/sdk";
 import { appSlice } from "store/reducers/appSlice";
-import {  getUserPanelData } from "store/apis";
+import { getLeaderboard, getUserFishes, getUserPanelData } from "store/apis";
 
 const DashBoardPage = () => {
-  const { activeTab, /*, initDataRow */ lang} = useAppSelector(
+  const { activeTab, /*, initDataRow */ lang } = useAppSelector(
     (state) => state.appSlice
   );
   //const { initDataRaw } = retrieveLaunchParams();
@@ -33,48 +33,73 @@ const DashBoardPage = () => {
     if (initDataRaw) {
       dispatch(setInitDataRow(initDataRaw!));
       dispatch(getUserPanelData({ tma: initDataRaw! }));
+      dispatch(getLeaderboard({ tma: initDataRaw! }));
+      dispatch(getUserFishes({ tma: initDataRaw! }));
     }
-  }, [initDataRaw]);
 
+  }, [initDataRaw]);
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const onLoadEffect = () => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    setTimeout(() => {
+      setIsError(true);
+    }, 5000);
+  };
+  useEffect(onLoadEffect, []);
   return (
     <>
       <div className={s.page}>
-        {!localStorage.getItem("firstVisit0") ? (
-          <FirstVizit />
-        ) : (
+        {isLoading ?
           <>
-            {lang ? (
-              <>
-                <Header />
-                {activeTab === "1" && (
-                  <>
-                    <CheckIn />
-                  </>
-                )}
-                {activeTab === "0" && (
-                  <>
-                    <Game />
-                    {/* <Wallet /> */}
-                  </>
-                )}
-
-                {activeTab === "3" && (
-                  <>
-                    <ReferralLink />
-                  </>
-                )}
-
-                <BottomMenu />
-              </>
+            <img src={title} className={s.title} />
+            <Loader />
+          </>
+          :
+          <>
+            {!localStorage.getItem("firstVisit") ? (
+              <FirstVizit />
             ) : (
               <>
-                {" "}
-                <img src={title} className={s.title} />
-                <Loader />
+                {lang ? (
+                  <>
+                    <Header />
+                    {activeTab === "1" && (
+                      <>
+                        <CheckIn />
+                      </>
+                    )}
+                    {activeTab === "0" && (
+                      <>
+                        <Game />
+                        {/* <Wallet /> */}
+                      </>
+                    )}
+
+                    {activeTab === "3" && (
+                      <>
+                        <ReferralLink />
+                      </>
+                    )}
+
+                    <BottomMenu />
+                  </>
+                ) : (
+                  <>
+
+                    {" "}
+                    <img src={title} className={s.title} />
+                    <Loader />
+                  </>
+                )}
               </>
             )}
           </>
-        )}
+        }
       </div>
     </>
   );
