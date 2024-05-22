cc93e9adf97f46495be886ec3bee4e2610fb6f4f
apashintsev <av.pashintsev@gmail.com>
2024-05-22 22:25:54 +0300

    ++

--- src/features/game/ui/index.tsx
+++ src/features/game/ui/index.tsx
@@ -39,7 +39,7 @@ export const Game = () => {
     if (initDataRow) {
       dispatch(getActiveUsersPack({ tma: initDataRow! }));
     }
-  }, [/*initDataRow,*/ userPackNames, packsCount]);
+  }, [initDataRow, packsCount]);
 
   return (
     <div className={s.conatiner}>
