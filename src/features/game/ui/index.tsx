134382c2bb64ba60d55441e09ae1900bbc463066
apashintsev <av.pashintsev@gmail.com>
2024-05-22 19:58:33 +0300

    dynamic bait

--- src/features/game/ui/index.tsx
+++ src/features/game/ui/index.tsx
@@ -4,57 +4,72 @@ import fh2 from "./assets/fh2.svg";
 import fh3 from "./assets/fh3.svg";
 import corn from "./assets/corn.gif";
 import bread from "./assets/bread.gif";
-// import burger from "./assets/burger.gif";
-// import pizza from "./assets/pizza.gif";
-// import potato from "./assets/potato.gif";
+import burger from "./assets/burger.gif";
+import pizza from "./assets/pizza.gif";
+import potato from "./assets/potato.gif";
 import worm from "./assets/worm.gif";
 import { useEffect } from "react";
 import { getActiveUsersPack } from "store/apis";
 import { useAppDispatch, useAppSelector } from "hooks/redux";
 
 export const Game = () => {
-    // const baitItems = {
-    //     'bread': bread,
-    //     'corn': corn,
-    //     'worm': worm,
-    //     'burger': burger,
-    //     'pizza': pizza,
-    //     'potato': potato
+  const baitItems: { [key: string]: string } = {
+    bread: bread,
+    corn: corn,
+    worm: worm,
+    burger: burger,
+    pizza: pizza,
+    potato: potato,
+  };
+  const getPicture = (name: string) => {
+    return baitItems[name];
+  };
+  const dispatch = useAppDispatch();
 
-    // }
+  const { userPackNames, initDataRow, packsCount } = useAppSelector(
+    (state) => state.appSlice
+  );
+  useEffect(() => {
+    if (initDataRow) {
+      dispatch(getActiveUsersPack({ tma: initDataRow! }));
+    }
+  }, [initDataRow, userPackNames, packsCount]);
 
-    const dispatch = useAppDispatch();
- 
-    const { userPackNames, initDataRow, packsCount } = useAppSelector((state) => state.appSlice);
-    useEffect(() => {
-      if (initDataRow) {
-        dispatch(getActiveUsersPack({ tma: initDataRow! }));
-      }
-    }, [initDataRow, userPackNames, packsCount]);
-  
-    return (
-      <div className={s.conatiner}>
-        <div className={s.fishhooks}>
-          <div className={s.hook_item}>
-            <img src={fh1} alt="" className={s.hooks} />
-            {userPackNames?.find((x: string) => x.toLowerCase() == "corn") && (
-              <img src={corn} alt="" className={s.hook_bait} />
-            )}
-          </div>
+  return (
+    <div className={s.conatiner}>
+      <div className={s.fishhooks}>
+        <div className={s.hook_item}>
+          <img src={fh1} alt="" className={s.hooks} />
+          {userPackNames.length > 0 && (
+            <img
+              src={getPicture(userPackNames[0])}
+              alt=""
+              className={s.hook_bait}
+            />
+          )}
+        </div>
 
-          <div className={s.hook_item}>
-            <img src={fh2} alt="" className={s.hooks} />
-            {userPackNames?.find((x: string) => x.toLowerCase() == "bread") && (
-              <img src={bread} alt="" className={s.hook_bait} />
-            )}
-          </div>
-          <div className={s.hook_item}>
-            <img src={fh3} alt="" className={s.hooks} />
-            {userPackNames?.find((x: string) => x.toLowerCase() == "worm") && (
-              <img src={worm} alt="" className={s.hook_bait} />
-            )}
-          </div>
+        <div className={s.hook_item}>
+          <img src={fh2} alt="" className={s.hooks} />
+          {userPackNames.length > 1 && (
+            <img
+              src={getPicture(userPackNames[1])}
+              alt=""
+              className={s.hook_bait}
+            />
+          )}
+        </div>
+        <div className={s.hook_item}>
+          <img src={fh3} alt="" className={s.hooks} />
+          {userPackNames.length > 2 && (
+            <img
+              src={getPicture(userPackNames[2])}
+              alt=""
+              className={s.hook_bait}
+            />
+          )}
         </div>
       </div>
-    );
+    </div>
+  );
 };
