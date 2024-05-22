5e02fab6299bccb6c5c19c9e7a968287783d979e
apashintsev <av.pashintsev@gmail.com>
2024-05-22 16:09:44 +0300

    ++

--- src/features/checkIn/ui/components/Gm-claim/index.tsx
+++ src/features/checkIn/ui/components/Gm-claim/index.tsx
@@ -21,7 +21,7 @@ export const GmClaim: FC<IProps> = ({ fishNumber, canClaim, isClaimed }) => {
   );
 
   const dispatch = useAppDispatch();
-  const diff = nextFishDate.getTime() - new Date(Date.now()).getTime();
+  const diff = nextFishDate.getTime(); /* - new Date(Date.now()).getTime()*/
   //const add = fishNumber - userFishesCount;
 
   const onClaim = () => {
