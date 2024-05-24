b549b4a4903f193b520eba29f00f76a89cdc07f4
apashintsev <av.pashintsev@gmail.com>
2024-05-24 19:08:58 +0300

    args

--- src/hooks/useUpdateBalance.ts
+++ src/hooks/useUpdateBalance.ts
@@ -13,7 +13,6 @@ export const useUpdateBalance = () => {
 
   useEffect(() => {
     if (initDataRow) {
-      console.log({ initDataRow });
       const newConnection = new HubConnectionBuilder()
         .withUrl("https://bhjkoihtfge89y23tghio.xyz/balance", {
           withCredentials: false,
@@ -38,10 +37,11 @@ export const useUpdateBalance = () => {
       connection
         .start()
         .then(() => {
-          connection.on("PaymentConfirmed", (balance, userId) => {
-            console.log({ balance });
-            console.log({ userId });
-            dispatch(setBalance(balance));
+          connection.on("PaymentConfirmed", (...args: any[]) => {
+            console.log({ args });
+            //console.log({ balance });
+            //console.log({ userId });
+            //dispatch(setBalance(balance));
             toast.success("Баланс пополнен успешно!");
           });
         })
