8a8b1352a5f1351173478ccb08331631ba412b7a
apashintsev <av.pashintsev@gmail.com>
2024-05-24 14:26:00 +0300

    +

--- src/features/checkIn/ui/components/slider/index.tsx
+++ src/features/checkIn/ui/components/slider/index.tsx
@@ -40,7 +40,7 @@ export const Slider: React.FC = () => {
         <div className={s.embla__container}>
           {SLIDES.map(
             (index) =>
-              index >= userFishesCount && (
+              index + 1 >= userFishesCount && (
                 <div
                   className={clsx(s.embla__slide, "shadow", {
                     [s.slide_disable]: allfishes.find((x) => x.id! - 1 == index)
